fc8fb80268cc572d9eb24e92f46b82671ce261b6
Nicolas <eb3us2@tutanota.com>
2026-05-18 18:17:11 -0300

    new plugins, lazy-lock

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,85 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  -- Noice
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  -- Lazygit
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+  {
+    'NachoNievaG/atac.nvim',
+    keys = {
+      { '<leader>at', '<cmd>Atac<cr>', desc = 'Atac' },
+    },
+    dependencies = { 'akinsho/toggleterm.nvim' },
+    config = function()
+      vim.env.ATAC_KEY_BINDINGS = '~/.config/atac/callixto/vim_key_bindings.toml'
+      require('atac').setup {
+        dir = '~/.config/atac/callixto/', -- By default, the dir will be set as /tmp/atac
+      }
+    end,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    keys = {
+      { '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', desc = 'ToggleTerm' },
+      { '<leader>tt', '<cmd>ToggleTerm direction=horizontal<cr>', desc = 'ToggleTerm' },
+      { '<leader>tv', '<cmd>ToggleTerm direction=vertical size=80<cr>', desc = 'ToggleTerm' },
+    },
+    config = function() require('toggleterm').setup {} end,
+  },
+  {
+    'Exafunction/windsurf.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    cond = function() return vim.env.CODEIUM == 'enabled' end,
+    event = 'BufEnter',
+    config = function()
+      require('codeium').setup {
+        virtual_text = {
+          enabled = true,
+          key_bindings = {
+            accept = '<C-a>',
+            accept_word = '<C-w>',
+            accept_line = '<C-s>',
+          },
+        },
+      }
+    end,
+  },
+}
