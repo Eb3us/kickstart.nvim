cf86c83f44dc78587ec2ad7944e8784c64b7e1c7
Nicolas <eb3us2@tutanota.com>
2026-06-04 03:26:27 -0300

    added reder-markdown plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -23,6 +23,13 @@ return {
     },
   },
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
     'Exafunction/windsurf.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
